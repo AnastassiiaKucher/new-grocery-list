d2306e7d26d4fb75ca6fd847efa4afae68515aa3
Anastasiia Kucher <anastasielisson@gmail.com>
2023-07-21 17:56:01 +0200

    Changes App.css/App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
   return (
     <div className="app">
       <div className='container'>
-      <img src={ image } width="200px" alt="shopping"/>
+      <img className='Man'  src={ image } width="200px" alt="shopping"/>
       </div>
       <div className="container">
       <h1>Grocery List</h1>
       </div>
       <GroceryList/>
       <div className='container'>
-      <img src={ imageTwo } width="200px" alt="man"/>
+      <img className='Man' src={ imageTwo } width="200px" alt="man"/>
       </div>
     </div>
   );
